459d34a77415dc749d1c6a3377ed63d317b01ba3
jpsiyu <jpsiyu@163.com>
2018-12-01 21:25:38 +0800

    update nodeframework

--- backend-nodeframework.js
+++ backend-nodeframework.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
-const { spawnProcess } = require('./lib/process.js')
+const { spawnProcess, execProcess } = require('./lib/process.js')
 const url = 'https://github.com/jpsiyu/node-framework.git'
-spawnProcess('git', ['clone', url, '.'])
+
+const rmGitReposity = () => {
+    console.log('rm git reposity')
+    execProcess('rm -rf .git')
+}
+spawnProcess('git', ['clone', url, '.'], rmGitReposity)
